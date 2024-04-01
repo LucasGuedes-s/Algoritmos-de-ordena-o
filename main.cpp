f2acbe78a59fe6ffd11f4672acc092595c9a7a27
Lucas Guedes <105815700+LucasGuedes-s@users.noreply.github.com>
2024-04-01 20:24:08 -0300

    Adicionando o InsertionSort main.cpp

--- main.cpp
+++ main.cpp
@@ -4,28 +4,60 @@
 
 using namespace std;
 
+
+void insertionSort(vector<int> &v){
+
+	int n = v.size();
+	int i, key, j;
+	
+	for(i=1; i<n; i++){
+		key = v[i];
+		j = i-1;
+		
+		while(j>=0 && v[j] > key){
+			v[j+1] = v[j];
+			j = j-1;
+		}
+		v[j+1] = key;
+	}
+}
+
+
 int main()
 {
     vector<int> vetor(10);
     
     
     //Preenchendo o vetor com valores aleatórios
+    
     for (int i = 0; i < vetor.size(); i++) {
-        vetor[i] = rand() % 500 + 1; /* Limitando os valores somente de 1 - 500 */
+        vetor[i] = rand() % 500 + 1; // Limitando os valores somente de 1 - 500
     }
     
     // Gerando cópias do vetor para serem testadas em cada algorítmo de ordenação
-    vector<int> bubbleSort = vetor;
-    vector<int> selectionSort = vetor;
-    vector<int> insertionSort = vetor;
-    vector<int> shellSort = vetor;
-    vector<int> quickSort = vetor;
-    vector<int> mergeSort = vetor;
     
-    /* Mostrar os valores do vetor
+    vector<int> v_bubbleSort = vetor;
+    vector<int> v_selectionSort = vetor;
+    vector<int> v_insertionSort = vetor;
+    vector<int> v_shellSort = vetor;
+    vector<int> v_quickSort = vetor;
+    vector<int> v_mergeSort = vetor;
+    
+    insertionSort(v_insertionSort);
+    
+    
+    //Mostrar os valores do vetor desordenado
+
     for(int j = 0; j < vetor.size(); j++){
         cout<<vetor[j]<<endl;
-    } */
+    }
+    cout<< "------------------"<<endl;
+    
+    //Mostrar os valores do vetor já ordenado! Esse for serve para confirmar se o vetor esta sendo ordenado
+    
+    for(int j = 0; j < v_insertionSort.size(); j++){
+        cout<<vetor[j]<<endl;
+    }
     
     return 0;
 }
